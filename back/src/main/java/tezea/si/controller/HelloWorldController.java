0f54ed92b752670d3ec0661d14217aadee6ee4e8
Nils <nils.richard@free.fr>
2021-02-22 14:06:40 +0100

    Adding photos upload back

--- back/src/main/java/tezea/si/controller/HelloWorldController.java
+++ back/src/main/java/tezea/si/controller/HelloWorldController.java
@@ -1,19 +1,28 @@
 package tezea.si.controller;
 
+import java.io.IOException;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import tezea.si.utils.FileUploadUtil;
 
 /**
  * A test controller to remove before production (Used in tests)
+ * 
  * @author Nils Richard
  *
  */
@@ -30,6 +39,20 @@ public class HelloWorldController {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
         return "Hello " + auth.getName();
+    }
+
+    @Operation(summary = "Test to upload photos")
+    @ApiResponses(value = { @ApiResponse(responseCode = "201", description = "A simple upload") })
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public ResponseEntity<?> uploadPhoto(@RequestParam("image") MultipartFile multipartFile) throws IOException {
+        logger.debug("Uploading a photo");
+        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+
+        String uploadDir = "user-photos/";
+
+        FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
 }
